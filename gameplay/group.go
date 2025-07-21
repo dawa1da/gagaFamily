bf54b34efe697a8fa4a596df9570e62f4f038646
bula.wsx <bula.wsx@come-future.com>
2025-07-21 17:36:36 +0800

    bbb

--- gameplay/group.go
+++ gameplay/group.go
@@ -2,40 +2,108 @@ package gameplay
 
 import (
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
-var girlSlice = []string{"圈姐", "真心", "闪闪", "47"}
-var boySlice = []string{"柯镇恶", "季富龙", "方震", "逍遥", "小何", "达芬奇"}
-
-func GetGroupHandler(w http.ResponseWriter, r *http.Request) {
-	selectedMap := make(map[string]struct{})
-	users := make([]string, 0, 5)
-
-	for len(users) < 2 {
-		user := girlSlice[rand.Intn(len(girlSlice))]
-		if _, ok := selectedMap[user]; !ok {
-			selectedMap[user] = struct{}{}
-			users = append(users, user)
-		}
-	}
-
-	for len(users) < 5 {
-		user := boySlice[rand.Intn(len(boySlice))]
-		if _, ok := selectedMap[user]; !ok {
-			selectedMap[user] = struct{}{}
-			users = append(users, user)
-		}
-	}
-
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("%s", users))
-	log.Println(fmt.Sprintf("随机玩家为:%s", users))
+type Group struct {
+	Users []*UserInfo `yaml:"users"`
+}
+type UserInfo struct {
+	Name       string   `yaml:"name"`
+	Position   string   `yaml:"position"`
+	BanList    []string `yaml:"banList"`
+	WeightList []string `yaml:"weightList"`
 }
 
-func GetLuckBoyHandler(w http.ResponseWriter, r *http.Request) {
-	user := boySlice[rand.Intn(len(boySlice))]
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("%s", user))
-	log.Println(fmt.Sprintf("随机幸运男孩为:%s", user))
+type Hero struct {
+	Positions map[string][]string `yaml:"hero"`
+}
+
+func GetGroupHandler(w http.ResponseWriter, r *http.Request) {
+	userFileBytes, _ := os.ReadFile("user.yaml")
+	group := new(Group)
+	_ = yaml.Unmarshal(userFileBytes, group)
+	heroFileBytes, _ := os.ReadFile("hero.yaml")
+	hero := new(Hero)
+	hero.Positions = make(map[string][]string)
+	_ = yaml.Unmarshal(heroFileBytes, hero.Positions)
+
+	if len(group.Users) != 10 {
+		_, _ = fmt.Fprintf(w, "配置文件里用户数量不为10")
+		return
+	}
+
+	positions := []string{"对抗路", "打野", "中路", "发育路", "游走"}
+	positions = append(positions, positions...)
+
+	//打乱一下玩家顺序，然后顺序分配分路就好了，实现随机分路
+	rand.Shuffle(len(group.Users), func(i, j int) {
+		group.Users[i], group.Users[j] = group.Users[j], group.Users[i]
+	})
+
+	for i, user := range group.Users {
+		group.Users[i].Position = positions[i]
+		userHeroPool := make([]string, len(hero.Positions[positions[i]]))
+		copy(userHeroPool, hero.Positions[positions[i]])
+
+		for _, weightHero := range user.WeightList {
+			if strings.Contains(weightHero, "*") {
+				ss := strings.Split(weightHero, "*")
+				if !contains(userHeroPool, ss[0]) {
+					continue
+				}
+
+				counts, _ := strconv.Atoi(ss[1])
+				for j := 0; j < counts; j++ {
+					userHeroPool = append(userHeroPool, ss[0])
+				}
+			} else {
+				if !contains(userHeroPool, weightHero) {
+					continue
+				}
+				userHeroPool = append(userHeroPool, weightHero)
+			}
+		}
+		for _, banHero := range user.BanList {
+			userHeroPool = removeString(userHeroPool, banHero)
+		}
+
+		_, _ = fmt.Fprintf(w, fmt.Sprintf("[%s]玩家为[%s]随机英雄为[%s]\n", user.Position, user.Name, getRandomString(userHeroPool)))
+		if i == 4 {
+			_, _ = fmt.Fprintf(w, fmt.Sprintf("\n"))
+		}
+	}
+}
+
+func contains(arr []string, target string) bool {
+	for _, item := range arr {
+		if item == target {
+			return true
+		}
+	}
+	return false
+}
+
+func getRandomString(arr []string) string {
+	if len(arr) == 0 {
+		return ""
+	}
+	index := rand.Intn(len(arr))
+	return arr[index]
+}
+
+func removeString(input []string, target string) []string {
+	var result []string
+	for _, str := range input {
+		if str != target {
+			result = append(result, str)
+		}
+	}
+	return result
 }
